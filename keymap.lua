83c696d838eab630112d2d3d5a66f99b7e7f2dbf
TestRunnerSRL <aaturpie@verizon.net>
2015-05-20 06:28:37 -0400

    InputDisplay and some bug fixes
    Added a input display.
    Fixed in superstarsagamodifyinputs with button swapping out of combat.
    Fixed in setkeymaps for keys that are multiword.
    Added a option in config for setting the game

--- keymap.lua
+++ keymap.lua
@@ -1,19 +1,16 @@
---This file contains the controller key mappings.
---This file can be set appropriately by running setkeymap.lua,
---or it can be manually edited - the names of keys can be found at
--- http://www.codeproject.com/Tips/73227/Keys-Enumeration-Win
-
 local keymap = {
-  J = "Left",
-  I = "Up",
-  L = "Right",
-  K = "Down",
-  U = "Start",
-  H = "Select",
-  D = "A",
-  F = "B",
-  E = "L",
-  R = "R"
+  ["J1 B6"] = "Start"
+,  ["J1 B4"] = "A"
+,  ["J1 B3"] = "B"
+,  ["J1 Y-"] = "Up"
+,  ["J1 Y+"] = "Down"
+,  ["J1 B7"] = "L"
+,  ["J1 B8"] = "R"
+,  ["J1 X-"] = "Left"
+,  ["J1 X+"] = "Right"
+,  ["J1 B5"] = "Select"
+,  ["J1 B2"] = "Y"
+,  ["J1 B1"] = "X"
 }
 
 return keymap
