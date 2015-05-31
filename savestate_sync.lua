c1086abb1cd91e7313e4a4bea422e3ebc2389653
TestRunnerSRL <aaturpie@verizon.net>
2015-05-31 06:12:32 -0400

    Changed resetting console to pressing power button
    Initial sync compares save batteries.
    Added frame offset.
    Quick testing showed no desync, but thorough testing is needed

--- savestate_sync.lua
+++ savestate_sync.lua
@@ -39,10 +39,8 @@ function savestate_sync.update_hash(slot)
 
     local save_hash = sha1.sha1(save_text)
     savestate_hashes[slot] = save_hash
-      print(slot .. ": " .. save_hash)
   else
     savestate_hashes[slot] = nil
-      print(slot .. ': nil')
   end
 end
 
@@ -55,31 +53,61 @@ event.onsavestate(savestate_sync.update_hash)
 --both players, and that both players' saves are the same.
 --returns true if the slot can be loaded
 --returns false and a reason for failure if not
-function savestate_sync.is_safe_to_loadslot(client_socket, slot)
-  if (savestate_hashes[slot] == nil) then
-    local reason = "Savestate file for slot " .. slot .. " does not exist."
-    messenger.send(client_socket, messenger.LOAD_FAIL, reason)
-    messenger.receive(client_socket)
-    return false, reason
-  end
+function savestate_sync.are_batteries_same(client_socket)
+  local save_hash = ""
 
-  messenger.send(client_socket, messenger.SAVE_HASH, slot, savestate_hashes[slot])
+  --construct the filename
+  local romname = gameinfo.getromname()
+  local systemname = emu.getsystemid()
+  local filename = config.path_to_root .. systemname .. "/SaveRAM/" .. romname .. ".SaveRAM"
 
-  --If we can load, see what the deal is with the other player
-  local received_message_type, received_data = messenger.receive(client_socket)
-  if (received_message_type == messenger.SAVE_HASH) then
-    local their_save_hash = received_data[2]
-    --check that the save states match
-    if (slot == received_data[1]) and (savestate_hashes[slot] == their_save_hash) then
-      return true
+  --check if it exists
+  if (file_exists(filename)) then
+    --construct a value that represents the save state
+    local save_text = ""
+
+    local f = assert(io.open(filename, "r"))
+    save_text = f:read("*all")
+    f:close()
+
+    local save_hash = sha1.sha1(save_text)
+
+    messenger.send(client_socket, messenger.SAVE_HASH, 0, save_hash)
+
+    --If we can load, see what the deal is with the other player
+    local received_message_type, received_data = messenger.receive(client_socket)
+    if (received_message_type == messenger.SAVE_HASH) then
+      local their_save_hash = received_data[2]
+      --check that the save states match
+      if (save_hash == their_save_hash) then
+        return true
+      else
+        return false, "Your batteries do not match!"
+      end
+    elseif  (received_message_type == messenger.LOAD_FAIL) then
+      local their_reason = received_data[1]
+      return false, "Other player would have an error loading:\n" .. their_reason
     else
-      return false, "Your saves at slot " .. slot .. " do not match!"
+      error("Unexpected message type received.")
     end
-  elseif  (received_message_type == messenger.LOAD_FAIL) then
-    local their_reason = received_data[1]
-    return false, "Other player would have an error loading:\n" .. their_reason
   else
-    error("Unexpected message type received.")
+    local reason = "Could not find save battery."
+    messenger.send(client_socket, messenger.LOAD_FAIL, reason)
+
+    --If we can't load, see what the deal is with the other player
+    local received_message_type, received_data = messenger.receive(client_socket)
+    if (received_message_type == messenger.SAVE_HASH) then
+      return false, reason
+    elseif  (received_message_type == messenger.LOAD_FAIL) then
+      local their_reason = received_data[1]
+      if (their_reason == "Could not find save battery.") then
+        return true
+      else
+        return false, "Other player would have an error loading:\n" .. their_reason
+      end
+    else
+      error("Unexpected message type received.")  
+    end
   end
 end
 
