d2a15315402e22c2087b60bdb1e831252664855c
Lumen Fairy <LumenTheFairy@gmail.com>
2015-06-04 21:46:31 -0500

    Fixed battery hash; Readme changes.

--- 2p1c/savestate_sync.lua
+++ 2p1c/savestate_sync.lua
@@ -76,10 +76,7 @@ function savestate_sync.are_batteries_same(client_socket)
   if (file_exists(filename)) then
     --construct a value that represents the save state
     local save_text = ""
-
-    local f = assert(io.open(filename, "r"))
-    save_text = f:read("*all")
-    f:close()
+    for line in io.lines(filename) do save_text = save_text .. line .. "\n" end
 
     local save_hash = sha1.sha1(save_text)
 
